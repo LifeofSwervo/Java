4b0185576ec3be63c7cb9ae7f9920b0e31fe523d
Swervo <82927987+LifeofSwervo@users.noreply.github.com>
2023-09-07 21:02:52 -0500

    Updated variables

--- Clarence_Thomas-Assignment1/src/ShapeCalculator.java
+++ Clarence_Thomas-Assignment1/src/ShapeCalculator.java
@@ -9,6 +9,8 @@ public class ShapeCalculator {
         Scanner input = new Scanner(System.in);
         String decision = "y";
         String output = "";
+
+        // Check if user wishes to repeat
         while (decision.equalsIgnoreCase("y")) {
             System.out.println("1. Area of Rectangle");
             System.out.println("2. Area of Triangle");
@@ -17,28 +19,37 @@ public class ShapeCalculator {
             System.out.print("Please select operations: ");
             int operation = input.nextInt(); // Parse operation selected by user
 
-            System.out.print("Enter Base: ");
-            double num1 = input.nextInt(); // Parse Base
 
-            System.out.print("Enter Height: ");
-            double num2 = input.nextInt(); // Parse Height
-
-            if (operation == 1) {
-                double rectangleArea = num1 * num2;
-                output = num1 + " * " + num2 + " = " + rectangleArea;
-            } else if (operation == 2) {
-                double triangleArea = (num1 * num2) / 2;
-                output = " ( " + num1 + " * " + num2 + " ) " + " = " + triangleArea;
-            } else if (operation == 3) {
-                System.out.print("Enter the second base: "); // Request 3rd variable for trapezoid
-                double num3 = input.nextInt();
-                double trapezoidArea = ((num1 + num3) / 2) * num2; // (0.5 * (Base(1) + Base(2))) * Height
+            if (operation == 1) { // If user selects rectangle
+                System.out.print("Enter length of the rectangle: ");
+                double num1 = input.nextInt(); // Parse Length
+                System.out.print("Enter width of the rectangle: ");
+                double num2 = input.nextInt(); // Parse Width
+                double rectangleArea = num1 * num2; // Calculate's area of rectangle
+                output = "The area of the rectangle is: " + rectangleArea;
+            } else if (operation == 2) { // If user selects triangle
+                System.out.print("Enter base of the triangle: ");
+                double num1 = input.nextInt(); // Parse Base
+                System.out.print("Enter height of the triangle: ");
+                double num2 = input.nextInt(); // Parse Height
+                double triangleArea = (num1 * num2) / 2; // Calculate's area of triangle
+                output = "The area of the triangle is: " + triangleArea;
+            } else if (operation == 3) { // If user selects trapezoid
+                System.out.print("Enter base of the trapezoid: ");
+                double num1 = input.nextInt(); // Parse Base
+                System.out.print("Enter height of the trapezoid: ");
+                double num2 = input.nextInt(); // Parse Height
+                System.out.print("Enter the second base of the trapezoid: "); // Request 3rd variable for trapezoid
+                double num3 = input.nextInt(); // Parse second Base value
+                double trapezoidArea = ((num1 + num3) / 2) * num2; // Calculate's area of trapezoid
                 output = "The area of the trapezoid is: " + trapezoidArea;
-            } else if (operation == 4) {
+            } else if (operation == 4) { // If user selects circle
                 System.out.println("Enter radius of Circle: ");
-                double radius = input.nextInt();
-                double circleArea = (radius * radius) * 3.14;
+                double radius = input.nextInt(); // Parse radius of circle
+                double circleArea = (radius * radius) * 3.14; // Calculate's area of circle
                 output = "The area of the circle is: " + circleArea;
+            } else {
+                System.out.println("Incorrect Operation, press \"y\" to retry or \"n\" to close the calculator.);
             }
 
             System.out.println("\n" + output); // Display Result
@@ -46,7 +57,8 @@ public class ShapeCalculator {
             System.out.println("\nContinue (y/n)? ");
             decision = input.next();
         }
-        if (decision.equalsIgnoreCase("n")) { // After user has exited the program
+        // Checks if user wishes to end program
+        if (decision.equalsIgnoreCase("n")) {
             System.out.println("Have a great day, Goodbye! ");
         }
     }
