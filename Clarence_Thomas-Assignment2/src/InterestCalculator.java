3213aa7b8cde63dc8d2e17a11b0fc8e9cb06bb4b
LifeofSwervo <82927987+LifeofSwervo@users.noreply.github.com>
2023-09-11 16:24:02 -0500

    Interest Calculator setup

---
+++ Clarence_Thomas-Assignment2/src/InterestCalculator.java
@@ -0,0 +1,15 @@
+// @author: Clarence Thomas
+// CLASS: Info 1521 WA
+// ASSIGMENT: Assignment 1
+// @version: September 18th, 2023
+// RESOURCES:
+
+// EXPLANATION:
+
+public class InterestCalculator
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
