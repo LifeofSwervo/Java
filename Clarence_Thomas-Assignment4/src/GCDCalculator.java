2201c9aff3de35ad777f74b8e42d30a18299d295
Swervo <82927987+LifeofSwervo@users.noreply.github.com>
2023-09-29 02:59:22 -0500

    Initial commit

--- Clarence_Thomas-Assignment4/src/GCDCalculator.java
+++ Clarence_Thomas-Assignment4/src/GCDCalculator.java
@@ -14,7 +14,32 @@ public class GCDCalculator
         while (decision.equalsIgnoreCase("y"))
         {
             System.out.print("Enter the first number: ");
+            int firstNumberInput = input.nextInt();
+
             System.out.print("Enter the second number: ");
+            int secondNumberInput = input.nextInt();
+
+            //Calculation
+            while (firstNumberInput != 0)
+            {
+                while (secondNumberInput >= firstNumberInput)
+                {
+                    secondNumberInput -= firstNumberInput;
+                }
+                int temp = secondNumberInput;
+                secondNumberInput = firstNumberInput;
+                firstNumberInput = temp;
+            }
+            System.out.println(secondNumberInput);
+
+
+            System.out.print("Continue (y/n)? ");
+            decision = input.next(); // Check if user wishes to continue
+        }
+
+        if (decision.equalsIgnoreCase("n"))
+        {
+            System.out.println("Have a great day, Goodbye! ");
         }
     }
 }
