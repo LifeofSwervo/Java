9527253478b1fa6ec492aba6d170bdbfab1636b9
LifeofSwervo <82927987+LifeofSwervo@users.noreply.github.com>
2023-09-29 02:14:30 -0500

    Commit

---
+++ Clarence_Thomas-Assignment4/src/GCDCalculator.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class GCDCalculator
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to Clarence's Greatest Common Denominator Calculator" + "\n");
+
+        // Declare variables
+        Scanner input = new Scanner(System.in);
+        String decision = "y";
+
+        // While user wishes to continue
+        while (decision.equalsIgnoreCase("y"))
+        {
+            System.out.print("Enter the first number: ");
+            System.out.print("Enter the second number: ");
+        }
+    }
+}
