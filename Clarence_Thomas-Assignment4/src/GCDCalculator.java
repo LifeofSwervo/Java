6a399082cfd898aeefe8b14fd6fac0f7ac65ada0
Swervo <82927987+LifeofSwervo@users.noreply.github.com>
2023-09-30 02:16:47 -0500

    Notes

--- Clarence_Thomas-Assignment4/src/GCDCalculator.java
+++ Clarence_Thomas-Assignment4/src/GCDCalculator.java
@@ -14,19 +14,19 @@ public class GCDCalculator
         while (decision.equalsIgnoreCase("y"))
         {
             System.out.print("Enter the first number: ");
-            int firstNumberInput = input.nextInt();
+            int firstNumberInput = input.nextInt(); // Get users first number
 
             System.out.print("Enter the second number: ");
-            int secondNumberInput = input.nextInt();
+            int secondNumberInput = input.nextInt(); // Get users second number
 
             //Calculation
-            while (firstNumberInput != 0)
+            while (firstNumberInput != 0) // While first number is not 0
             {
-                while (secondNumberInput >= firstNumberInput)
+                while (secondNumberInput >= firstNumberInput) // Loop until second number is no longer more than or equal to first
                 {
-                    secondNumberInput -= firstNumberInput;
+                    secondNumberInput -= firstNumberInput; // Subtract first number from second
                 }
-                int temp = secondNumberInput;
+                int temp = secondNumberInput; // initilze a temp equaling the new-found second number (from loop)
                 secondNumberInput = firstNumberInput;
                 firstNumberInput = temp;
             }
@@ -37,6 +37,7 @@ public class GCDCalculator
             decision = input.next(); // Check if user wishes to continue
         }
 
+        // If user no longer wishes to continue
         if (decision.equalsIgnoreCase("n"))
         {
             System.out.println("Have a great day, Goodbye! ");
