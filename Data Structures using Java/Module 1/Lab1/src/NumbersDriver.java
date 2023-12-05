05f756afb9fd555a8bb06bcd217a06b9c0c7d87e
Swervo <82927987+LifeofSwervo@users.noreply.github.com>
2023-12-05 17:42:19 -0600

    Update NumbersDriver.java

--- Data Structures using Java/Module 1/Lab1/src/NumbersDriver.java
+++ Data Structures using Java/Module 1/Lab1/src/NumbersDriver.java
@@ -2,17 +2,17 @@ public class NumbersDriver
 {
     public static void main(String[] args)
     {
-        // Instance Numbers Object
+        // Instance Numbers Object with 10 elements
         Numbers numbersObject = new Numbers(10);
         // Display method on object
         System.out.println("Initial Array: ");
         numbersObject.display();
         // SetValue method on numbers object (0 - 100)
         numbersObject.setValue(0, 50);
-        numbersObject.setValue(25, 90);
+        numbersObject.setValue(9, 90);
         // getValue Method displaying values at certain indices.
-        System.out.println("Index 0: " + numbersObject.getValue(0));
-        System.out.println("Index 90: " + numbersObject.getValue(90));
+        System.out.println("First number of array (Index 0): " + numbersObject.getValue(0));
+        System.out.println("Last number of array (Index 9): " + numbersObject.getValue(9));
         // getLength, showing length of the array.
         System.out.println("Array Length: " + numbersObject.getLength());
         // Display array
