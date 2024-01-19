f87557397279d807acadd8eb6f93797bba344af2
LifeofSwervo <82927987+LifeofSwervo@users.noreply.github.com>
2024-01-18 22:49:02 -0600

    Working Version.

--- Data Structures using Java/Test/Test/src/Node.java
+++ Data Structures using Java/Test/Test/src/Node.java
@@ -1,16 +1,11 @@
 class Node
 {
-    public int iData; // data item (key)
-    public double dData; // data item
+    public char data; //
     public Node leftChild; // this node’s left child
     public Node rightChild; // this node’s right child
     public void displayNode() // display ourself
     {
-        System.out.print("{");
-        System.out.print(iData);
-        System.out.print(", ");
-        System.out.print(dData);
-        System.out.print("} ");
+        System.out.println(data);
     }
 
 } // end class Node
