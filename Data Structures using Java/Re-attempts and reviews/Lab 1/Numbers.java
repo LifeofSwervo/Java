016b324062934289836ec71f10776d7d7dceea27
LifeofSwervo <82927987+LifeofSwervo@users.noreply.github.com>
2024-02-27 00:19:52 -0600

    Lab instructions.

--- Data Structures using Java/Re-attempts and reviews/Lab 1/Numbers.java
+++ Data Structures using Java/Re-attempts and reviews/Lab 1/Numbers.java
@@ -7,12 +7,26 @@ public class Numbers
         integerArray = new int[arrayLength];
     }
 
-    public void disply() 
+    public void display() 
     {
         for (int num : integerArray) 
         {
             System.out.print(num + " ");
         }
         System.out.println();
+    }
+
+    // Getters
+
+    public int getValue() 
+    {
+        try 
+        {
+            return integerArray[index];
+        } catch(ArrayIndexOutOfBoundsException e) 
+        {
+            System.out.println("Index out of bounds.");
+            return 0;
+        }
     }
 }
